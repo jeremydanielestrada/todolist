928d3279389a6afc33c8870ab157657f2bc61982
Jeremy Daniel Estrada <jeremydanielestrada2001@gmail.com>
2025-03-12 07:48:59 +0800

    new feature added

--- script.js
+++ script.js
@@ -16,10 +16,15 @@ const taskCreations = () => {
   const list = document.createElement("li");
   const box = document.createElement("input");
   const ul = document.querySelector("ul");
+  const editButton = document.createElement("button");
   let tracker = document.querySelector("#tracker");
   box.type = "checkbox";
+  editButton.textContent = "Edit";
+  editButton.classList.add("btn", "btn-sm", "mx-2");
+
   list.appendChild(box);
   list.appendChild(text);
+  list.appendChild(editButton);
   ul.appendChild(list);
   box.style.marginRight = "10px";
   taskList.push(taskValue);
@@ -31,9 +36,20 @@ const taskCreations = () => {
       list.classList.add("fade-out");
       setTimeout(() => {
         list.remove();
+        //tracks the list
         taskList = taskList.filter((task) => task !== taskValue);
         tracker.innerHTML = taskList.length;
       }, 1000);
+    }
+  });
+
+  editButton.addEventListener("click", function () {
+    const newTaskValue = prompt("Edit your task:", taskValue);
+    if (newTaskValue) {
+      text.nodeValue = newTaskValue;
+      taskList = taskList.map((task) =>
+        task === taskValue ? newTaskValue : task
+      );
     }
   });
 };
